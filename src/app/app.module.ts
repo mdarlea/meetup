aa90f62363ed688077bd1d1ffdace9c5a1b928e2
Michelle Darlea <mdarlea@gmail.com>
2019-03-13 07:48:09 -0400

    removed google api key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { MapModule } from './shared/sw-map/map.module';
     CoreModule,
     ScrollToModule.forRoot(),
     MapModule.forRoot({
-      mapsApiKey: 'AIzaSyAHI8Ipk9hdcaX9ZZxBi4ve1ZuJxfTQ61Q'
+      mapsApiKey: 'xxx'
     }),
     SharedModule
   ],
