317078108f4ce8027f7dc32b8945b75a69bbe8d7
Michelle Darlea <mdarlea@gmail.com>
2019-05-01 06:17:33 -0400

    routing

--- src/app/meetings/meetings-routing.module.ts
+++ src/app/meetings/meetings-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { JqxSchedulerComponent} from './jqx-scheduler/jqx-scheduler.component';
 import {MeetingsComponent} from './meetings-root/meetings.component';
 import { JqxSchedulerTestComponent} from './jqx-scheduler-test/jqx-scheduler-test.component';
+import { AuthGuard } from '../auth.guard';
 // import { TestTimeComponent} from './test-time/test-time.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MeetingsComponent,
+    canActivate: [AuthGuard],
     children: [
       {path: '', redirectTo: 'jqx-scheduler', pathMatch: 'full'},
       {path: 'jqx-scheduler', component: JqxSchedulerComponent}
