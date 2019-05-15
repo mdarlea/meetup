4ccce147470a8a19b52d62a32baa6278e539644a
Michelle Darlea <mdarlea@gmail.com>
2019-05-15 12:05:33 -0400

    unit tests

--- src/app/meetings/jqx-scheduler/jqx-scheduler.component.spec.ts
+++ src/app/meetings/jqx-scheduler/jqx-scheduler.component.spec.ts
@@ -1,23 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
 import { SchedulerModule } from 'sw-scheduler';
-
+import { of } from 'rxjs';
+// import 'jasmine';
 
 import { JqxSchedulerComponent } from './jqx-scheduler.component';
 import { SharedModule } from '../../shared/shared.module';
 import { CoreModule } from '../../core/core.module';
 import { EventsQueryService } from '../shared/events-query.service';
 import { ApplicationInitStatus } from '@angular/core';
-import { AuthService } from '../../account/shared/auth.service';
-import { LoginViewModel } from '../../account/shared/login-view.model';
+import { TimeRangeDto } from '../../core/models/time-range-dto';
+import { EventDto } from '../../core/models/event-dto';
+import moment = require('moment');
 
 describe('JqxSchedulerComponent', () => {
   let component: JqxSchedulerComponent;
+  let debugElement: DebugElement;
   let fixture: ComponentFixture<JqxSchedulerComponent>;
+  let eventsQuerySvc: EventsQueryService;
+  let eventsQuerySvcSpy: jasmine.Spy;
   let originalTimeout;
 
   beforeEach(() => {
     originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
   });
 
   beforeEach(async(() => {
@@ -29,8 +35,7 @@ describe('JqxSchedulerComponent', () => {
         CoreModule
       ],
       providers: [
-        EventsQueryService,
-        AuthService
+        EventsQueryService
       ]
     })
     .compileComponents();
@@ -42,15 +47,40 @@ describe('JqxSchedulerComponent', () => {
     TestBed.get(ApplicationInitStatus).donePromise;
   }));
 
-  beforeEach(async(() => {
-    const service = TestBed.get(AuthService);
-    const vm = new LoginViewModel('testor@testing.com', 'Mihaela@777', false);
-    service.login(vm).subscribe();
-  }));
 
   beforeEach(async(() => {
     fixture = TestBed.createComponent(JqxSchedulerComponent);
+
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+
+    eventsQuerySvc = debugElement.injector.get(EventsQueryService);
+    eventsQuerySvcSpy = spyOn<any>(eventsQuerySvc, 'findWeeklyEvents').and.callFake(() => {
+      const startTime = new Date();
+      startTime.setHours(startTime.getHours() + 1);
+
+      const endTime = new Date();
+      endTime.setHours(endTime.getHours() + 2);
+
+      const dto = {
+        id: 1,
+        name: 'Planning',
+        instructor: 'John Doe',
+        addressId: 1,
+        userId: '123456',
+        startTime: moment(startTime).format(),
+        endTime: moment(endTime).format(),
+        repeat: false,
+        recurrencePattern: null,
+        recurrenceException: null,
+        endRecurrenceTime: null,
+        venueId: null
+      };
+      return (of([dto]));
+
+    });
+
+
     fixture.detectChanges();
     fixture.whenStable();
   }));
